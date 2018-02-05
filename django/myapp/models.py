2eef54fa290ff3e2144e291f2f06d07a00971ca4
Shin Dongjin <djshin2000@gmail.com>
2018-02-05 14:07:41 +0900

    Person model 추가

--- django/myapp/models.py
+++ django/myapp/models.py
@@ -4,11 +4,28 @@ from django.db import models
 class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    instrument = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=50, blank=True)
 
 
 class Album(models.Model):
     artist = models.ForeignKey(Musician, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    release_date = models.DateField()
+    release_date = models.DateField(blank=True, null=True)
     num_stars = models.IntegerField()
+
+
+class Person(models.Model):
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+    def __str__(self):
+        return '{name} (pk: {pk}, 셔츠 사이즈: {shirt_size})'.format(
+            name=self.name,
+            pk=self.pk,
+            shirt_size=self.get_shirt_size_display()
+        )
