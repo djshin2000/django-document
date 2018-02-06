3f2aa9dd0ac02e288ea6991deb670076ab9e6e72
Shin Dongjin <djshin2000@gmail.com>
2018-02-06 11:04:06 +0900

    model 패키지에 self.py 생성

--- django/many_to_many/admin.py
+++ django/many_to_many/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from many_to_many.models import Pizza, Topping, Post, User, PostLike
+from .models import (Pizza, Topping, Post, User, PostLike, FacebookUser)
 
 admin.site.register(Pizza)
 admin.site.register(Topping)
 admin.site.register(Post)
 admin.site.register(User)
 admin.site.register(PostLike)
+admin.site.register(FacebookUser)
