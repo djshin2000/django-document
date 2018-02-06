6df06ae745c014ece8df9ca735d466d87717e11f
Shin Dongjin <djshin2000@gmail.com>
2018-02-06 11:40:13 +0900

    self.py FacebooUser 출력 결과값 수정

--- django/many_to_many/models/self.py
+++ django/many_to_many/models/self.py
@@ -10,4 +10,16 @@ class FacebookUser(models.Model):
     friends = models.ManyToManyField('self')
 
     def __str__(self):
-        return self.name
+        # 출력 결과 >> 이한영 (친구: 박보영, 아이유)
+
+        # list comprehensions 사용
+        return '{name} (친구: {friends})'.format(
+            name=self.name,
+            friends=', '.join([friend.name for friend in self.friends.all()])
+        )
+
+        # Manager 의 value_list 사용
+        # return '{name} (친구: {friends})'.format(
+        #     name=self.name,
+        #     friends=', '.join(self.friends.value_list('name', flat=True))
+        # )
